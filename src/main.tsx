38a2a6252883d384d7efa6c47fd48fbbdf102c53
Siddharth Roy <siddharthroy36912@gmail.com>
2022-09-18 17:36:24 +0530

    Did stuffs

--- src/main.tsx
+++ src/main.tsx
@@ -6,22 +6,26 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import store from './store'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <div className="dark-theme">
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<>Homepage</>}/>
-        <Route path="/app" element={<App/>}>
-          <Route index element={<>Dashboard</>}/>
-          <Route path="todo" element={<>Todo</>}/>
-          <Route path="expense" element={<>Expense</>}/>
-          <Route path="notes" element={<>Notes</>}/>
-          <Route path="calendar" element={<>Calendar</>} />
-          <Route path="calculator" element={<>Calculator</>} />
-          <Route path="noise" element={<>Noise</>}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </div>
+  <Provider store={store}>
+    <div className="dark-theme">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<>Homepage</>}/>
+          <Route path="/app" element={<App/>}>
+            <Route index element={<>Dashboard</>}/>
+            <Route path="todo" element={<>Todo</>}/>
+            <Route path="expense" element={<>Expense</>}/>
+            <Route path="notes" element={<>Notes</>}/>
+            <Route path="calendar" element={<>Calendar</>} />
+            <Route path="calculator" element={<>Calculator</>} />
+            <Route path="noise" element={<>Noise</>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  </Provider>
 )
